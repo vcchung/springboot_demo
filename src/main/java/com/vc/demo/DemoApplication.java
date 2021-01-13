a8ea5002297a54149e9edcfa7e74566bc78ae5eb
vcchung <vcchung@gmail.com>
2021-01-13 11:48:12 +0800

    add Dockerfile

--- src/main/java/com/vc/demo/DemoApplication.java
+++ src/main/java/com/vc/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("This  is a sample Demo Application for Vincent");
+
 	}
 
 }
